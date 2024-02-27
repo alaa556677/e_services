de40d2a3e04172c779c1c94296258a890b885bf9
alaa55 <alaabaker166@gmail.com>
2024-02-27 16:48:13 +0200

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:e_services/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/bloc_observer/bloc_observer.dart';
 import 'features/home/presentation/pages/base_screen.dart';
+import 'firebase_options.dart';
 
-void main(){
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
